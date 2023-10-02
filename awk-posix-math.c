9af6cd294142b70e5d9d6d3bc224ca63d49e3562
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-10-02 13:31:31 +0330

    added signature to math functions

--- awk-posix-math.c
+++ awk-posix-math.c
@@ -1,47 +1,56 @@
+static inline awk_numeric_t
 awk_atan2(awk_numeric_t y, awk_numeric_t x)
 {
-	return atan2l((long double)y, (long double)x);
+	return (awk_numeric_t) atan2l((long double)y, (long double)x);
 }
 
+static inline awk_numeric_t
 awk_cos(awk_numeric_t x)
 {
-	return cosl((long double)x);	
+	return (awk_numeric_t) cosl((long double)x);	
 }
 
+static inline awk_numeric_t
 awk_sin(awk_numeric_t x)
 {
-	return sinl((long double)x);
+	return (awk_numeric_t) sinl((long double)x);
 }
 
+static inline awk_numeric_t
 awk_exp(awk_numeric_t x)
 {
-	return expl((long double)x);
+	return (awk_numeric_t) expl((long double)x);
 }
 
+static inline awk_numeric_t
 awk_log(awk_numeric_t x)
 {
-	return logl((long double)x);
+	return (awk_numeric_t) logl((long double)x);
 }
 
+static inline awk_numeric_t
 awk_sqrt(awk_numeric_t x)
 {
-	return sqrtl((long double)x);
+	return (awk_numeric_t) sqrtl((long double)x);
 }
 
 
+static inline awk_numeric_t
 awk_int(awk_numeric_t x)
 {
-	return (intmax_t)((long double)x);
+	return (awk_numeric_t) (intmax_t)((long double)x);
 }
 
+static inline awk_numeric_t
 awk_rand(void)
 {
-	return rand();
+	return (awk_numeric_t) rand();
 }
 
+static inline awk_numeric_t
 awk_srand(awk_numeric_t seed)
 {
-	return srand((uintmax_t)seed);
+	return (awk_numeric_t) srand((uintmax_t)seed);
 }
 
 
